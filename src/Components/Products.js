9c06182435bed40e11561ce7555f5577304a5d5d
esteban.acevedo <esteban.acevedo.98@hotmail.com>
2020-11-05 18:48:39 -0300

    Commit

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -2,8 +2,6 @@ import React from 'react'
 
 export default class Products extends React.Component{
     render(){
-        return(
-            <h1>Products</h1>
-        )
+        return
     }
 }
