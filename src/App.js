eea4fba67418fe00985219e30ab761897318d332
esteban.acevedo <esteban.acevedo.98@hotmail.com>
2020-11-04 21:02:43 -0300

    Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
-import React from 'react'
-
+import React from 'react';
+import Home from './Components/Home';
+import Contact from './Components/Contact';
+import Products from './Components/Products';
 
 export default class App extends React.Component{
   constructor(){
---
+++ src/Components/Products.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default class Products extends React.Component{
+    
+}
