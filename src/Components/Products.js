894affe08d90d40ee6b13bcd4f8c33180dcf83b1
malu <luchiss@hotmail.com>
2020-11-05 18:45:38 -0300

    malu

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,5 +1,27 @@
 import React from 'react'
+import Product from './ProductItem'
 
 export default class Products extends React.Component{
+
     
+    render(){
+        return
+            <>
+                <ul className="list-group">
+                    {this.props}
+                    return(
+                        <Product  title = {product},
+                                  description={} , 
+                                  imgUrl={} , 
+                                  inStock= {}, 
+                                  price= {},
+                                  currency= {},
+                                  discountPrice={} />
+                                  )
+            
+
+        </ul>
+    </>
+    
+    }
 }
